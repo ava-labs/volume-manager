e27b0d44413c8b580d60e6db2a3b3d33d4ebaf1e
Gyuho Lee <gyuho.lee@avalabs.org>
2022-07-07 21:30:01 +0800

    release: update
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- scripts/build.release.sh
+++ scripts/build.release.sh
@@ -9,6 +9,8 @@ fi
 # "--bin" can be specified multiple times for each directory in "bin/*" or workspaces
 cargo build \
 --release \
---bin aws-volume-mounter
+--bin aws-volume-mounter \
+--bin aws-volume-provisioner
 
 ./target/release/aws-volume-mounter --help
+./target/release/aws-volume-provisioner --help
