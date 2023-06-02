3562ecbf74188f6ec2361da2f4812b5df8f74e33
Gyuho Lee <gyuho.lee@avalabs.org>
2023-06-02 03:17:12 -0700

    0.0.119
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- aws-volume-provisioner/src/command.rs
+++ aws-volume-provisioner/src/command.rs
@@ -78,7 +78,7 @@ $ aws-volume-provisioner \
                 .required(false)
                 .num_args(1)
                 .value_parser(value_parser!(u32))
-                .default_value("60"),
+                .default_value("20"),
         )
         .arg(
             Arg::new("ID_TAG_KEY")
@@ -237,7 +237,7 @@ pub async fn execute(opts: Flags) -> io::Result<()> {
     );
 
     let sleep_sec = if opts.initial_wait_random_seconds > 0 {
-        opts.initial_wait_random_seconds + (random_manager::u32() % 30)
+        opts.initial_wait_random_seconds + (random_manager::u32() % 20)
     } else {
         10
     };
@@ -265,12 +265,16 @@ pub async fn execute(opts: Flags) -> io::Result<()> {
 
     log::info!("fetching the tag value for {}", opts.ec2_tag_asg_name_key);
     let mut asg_tag_value = String::new();
-    for i in 0..30 {
-        log::info!("[{i}] fetching tags until ec2_tag_asg_name_key is found");
+    for i in 0..50 {
+        log::info!(
+            "[{i}] fetching tags until ec2_tag_asg_name_key '{}' is found",
+            opts.ec2_tag_asg_name_key
+        );
         let tags = ec2_manager
             .fetch_tags(&ec2_instance_id)
             .await
             .map_err(|e| Error::new(ErrorKind::Other, format!("failed fetch_tags {}", e)))?;
+
         for c in tags {
             let k = c.key().unwrap();
             let v = c.value().unwrap();
